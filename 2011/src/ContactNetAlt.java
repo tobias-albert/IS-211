a18b5cdefde7627222d9a1b1fd4ae03be2ef8f13
Tobias 720s <tobiasalbert97@gmail.com>
2019-05-06 21:35:13 +0200

    Fixed alt method for graph, using adjency lists instead of just one collection

--- 2011/src/ContactNetAlt.java
+++ 2011/src/ContactNetAlt.java
@@ -2,8 +2,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 
-public class ContactNet {
-
+public class ContactNetAlt {
     HashMap<Conversation, Integer> conversations;
     HashSet<Person> persons;
     int connectionThreshold = 5;
@@ -12,42 +11,44 @@ public class ContactNet {
         return this.connectionThreshold = connectionThreshold;
     }
 
-    public ContactNet() {
+    public ContactNetAlt() {
         conversations = new HashMap<>();
         persons = new HashSet<>();
     }
 
-    public void registerConversation(Person sender, Person receiver) {
+    public void registerConversationAlt(Person sender, Person receiver) {
         boolean alreadyExists = false;
         Conversation c = new Conversation(sender, receiver);
-        if (conversations.containsKey(c)) {
-            int amount = conversations.get(c);
-            conversations.put(c, amount + 1);
+        if (sender.conversations.containsKey(c)) {
+            c.amount = sender.conversations.get(c).amount + 1;
+            sender.conversations.put(c, c);
         }
         else {
-            conversations.put(c, 1);
-            sender.neighbours.add(receiver);
+            c.amount = 1; //not needed when constructor for Conversation sets it to 1
+            sender.conversations.put(c, c);
         }
     }
 
-    public boolean hasDirectConnection (Person p1, Person p2) {
+    public boolean hasDirectConnectionAlt (Person p1, Person p2) {
         Conversation c1 = new Conversation(p1, p2);
         Conversation c2 = new Conversation(p2, p1);
         try {
-            if (conversations.get(c1) >= connectionThreshold && conversations.get(c2) >= connectionThreshold) {
+            if (p1.conversations.get(c1).amount > connectionThreshold
+                    && p2.conversations.get(c2).amount > connectionThreshold) {
                 return true;
             }
         }
-        catch (NullPointerException ex) { //one or both conversations was not found
+        catch (NullPointerException ex) {//one or both conversations was not found
             return false;
         }
         return false;
     }
 
-    public LinkedList<Person> findDirectConnections (Person suspect) {
+    public LinkedList<Person> findDirectConnectionsAlt (Person suspect) {
         LinkedList<Person> connections = new LinkedList<Person>();
-        for (Person neighbour : suspect.neighbours) {
-            if (hasDirectConnection(suspect, neighbour)) {
+        for (Conversation c : suspect.conversations.keySet()) {
+            Person neighbour = c.receiver;
+            if (hasDirectConnectionAlt(suspect, neighbour)) {
                 connections.add(neighbour);
             }
         }
@@ -60,18 +61,19 @@ public class ContactNet {
      * @param connections the current connections we know about - for initial method call use an empty set or null
      * @return
      */
-    public LinkedList<Person> findIndirectConnections(Person suspect, HashSet<Person> connections) {
+    public LinkedList<Person> findIndirectConnectionsAlt(Person suspect, HashSet<Person> connections) {
         connections = connections == null ? new HashSet<Person>() : connections;
         //for all neighbours
-        for (Person neighbour : suspect.neighbours) {
+        for (Conversation c : suspect.conversations.keySet()) {
+            Person neighbour = c.receiver;
             //do they have direct connection (above threshold)
-            if (hasDirectConnection(suspect, neighbour)) {
+            if (hasDirectConnectionAlt(suspect, neighbour)) {
                 //if the set doesn't already contain them
                 if (!connections.contains(neighbour)) {
                     //add them
                     connections.add(neighbour);
                     //check their neighbours
-                    connections.addAll(findIndirectConnections(neighbour, connections));
+                    connections.addAll(findIndirectConnectionsAlt(neighbour, connections));
 
                     /*
                     //think this part is unnecessary if the recursive call is moved outside of loop
@@ -91,17 +93,13 @@ public class ContactNet {
         return new LinkedList<Person>(connections);
     }
 
-    public void printDirect(Person suspect) {
+    public void printDirectAlt(Person suspect) {
         System.out.println("Direct connections for \"" + suspect.name + "\"");
-        System.out.println(findDirectConnections(suspect));
+        System.out.println(findDirectConnectionsAlt(suspect));
     }
 
-    public void printIndirect(Person suspect) {
+    public void printIndirectAlt(Person suspect) {
         System.out.println("Indirect connections for \"" + suspect.name + "\"");
-        System.out.println(findIndirectConnections(suspect, new HashSet<Person>()));
+        System.out.println(findIndirectConnectionsAlt(suspect, new HashSet<Person>()));
     }
-
-
-
-
 }
--- 2011/src/Main.java
+++ 2011/src/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
 
     public static void main(String[] args) {
+        setup();
+        setupAlt();
+    }
+
+    public static void setup() {
         ContactNet c = new ContactNet();
         c.setThreshold(10);
         Random r = new Random();
@@ -32,4 +37,35 @@ public class Main {
         c.printDirect(person);
         c.printIndirect(person);
     }
+
+    public static void setupAlt() {
+        ContactNetAlt c = new ContactNetAlt();
+        c.setThreshold(5);
+        Random r = new Random();
+
+        Person Per = new Person("Per");
+        Person Pål = new Person("Pål");
+        Person Espen = new Person("Espen");
+        Person Kongen = new Person("Kongen");
+        c.persons.add(Per);
+        c.persons.add(Pål);
+        c.persons.add(Espen);
+        c.persons.add(Kongen);
+        for (int i = 0; i < 10; i++) {
+            c.registerConversationAlt(Per, Pål);
+            c.registerConversationAlt(Pål, Per);
+            c.registerConversationAlt(Per, Espen);
+            c.registerConversationAlt(Espen, Per);
+            c.registerConversationAlt(Espen, Kongen);
+            c.registerConversationAlt(Kongen, Espen);
+            c.registerConversationAlt(Kongen, Per);
+            c.registerConversationAlt(Per, Espen);
+        }
+
+        Person person = Per;
+        c.printDirectAlt(person);
+        c.printIndirectAlt(person);
+    }
+
 }
+
